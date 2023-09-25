30ecbcf89e89897ab5c2cac76639883dd3c50b16
MurataAoi <kc9185910@gmail.com>
2023-09-25 15:43:05 +0900

    c-m修正

--- customer.py
+++ customer.py
@@ -8,8 +8,9 @@ class Customer:
 
 ken = Customer(first_name="Ken", family_name="Tanaka")
 ken.full_name()  # "Ken Tanaka" という値を返す
-print(ken.full_name())
 
 tom = Customer(first_name="Tom", family_name="Ford")
 tom.full_name()  # "Tom Ford" という値を返す
+
+print(ken.full_name())
 print(tom.full_name())
