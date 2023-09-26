23c65505ec42a234022dd2328f92a73c28c7a763
MurataAoi <kc9185910@gmail.com>
2023-09-26 17:44:33 +0900

    c-m課題修正-2

--- customer.py
+++ customer.py
@@ -1,39 +1,39 @@
 # ----------------------c-4 start----------------------------------
-class Customer:
-    def __init__(self, first_name, family_name, age):
-        self.first_name = first_name
-        self.family_name = family_name
-        self.age = age
+# class Customer:
+#     def __init__(self, first_name, family_name, age):
+#         self.first_name = first_name
+#         self.family_name = family_name
+#         self.age = age
 
-    def full_name(self):
-        return f"{self.first_name} {self.family_name}"
+#     def full_name(self):
+#         return f"{self.first_name} {self.family_name}"
 
-    def entry_fee(self):
-        if self.age < 20:
-            return "1000"
-        elif self.age >= 65:
-            return "1200"
-        else:
-            return "1500"
+#     def entry_fee(self):
+#         if self.age < 20:
+#             return "1000"
+#         elif self.age >= 65:
+#             return "1200"
+#         else:
+#             return "1500"
 
-    def info_csv(self):
-        # csv_string = f"{self.first_name} {self.family_name},{self.age},{self.entry_fee()}"
-        csv_string = f"{self.full_name()},{self.age},{self.entry_fee()}"
-        return csv_string
+#     def info_csv(self):
+#         # csv_string = f"{self.first_name} {self.family_name},{self.age},{self.entry_fee()}"
+#         csv_string = f"{self.full_name()},{self.age},{self.entry_fee()}"
+#         return csv_string
 
 
-ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
-ken.info_csv()  # "Ken Tanaka,15,1000" という値を返す
+# ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+# ken.info_csv()  # "Ken Tanaka,15,1000" という値を返す
 
-tom = Customer(first_name="Tom", family_name="Ford", age=57)
-tom.info_csv()  # "Tom Ford,57,1500" という値を返す
+# tom = Customer(first_name="Tom", family_name="Ford", age=57)
+# tom.info_csv()  # "Tom Ford,57,1500" という値を返す
 
-ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
-ieyasu.info_csv()  # "Ieyasu Tokugawa,73,1200" という値を返す
+# ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+# ieyasu.info_csv()  # "Ieyasu Tokugawa,73,1200" という値を返す
 
-print(ken.info_csv())
-print(tom.info_csv())
-print(ieyasu.info_csv())
+# print(ken.info_csv())
+# print(tom.info_csv())
+# print(ieyasu.info_csv())
 # -------------------c-4 finish--------------------------------
 # -------------------c-3 start--------------------------------
 # class Customer:
@@ -71,7 +71,7 @@ print(ieyasu.info_csv())
 
 # -----------------c-2 start-----------------------------------
 # class Customer:
-#     def __init__(self, first_name,family_name,age):
+#     def __init__(self, first_name, family_name, age):
 #         self.first_name = first_name
 #         self.family_name = family_name
 #         self.age = age
@@ -79,14 +79,15 @@ print(ieyasu.info_csv())
 #     def full_name(self):
 #         return f"{self.first_name} {self.family_name}"
 
+
 # ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
 # ken.age  # 15 という値を返す
 
-# tom = Customer(first_name="Tom", family_name="Ford", age= 57)
-# tom.age # 57 という値を返す
+# tom = Customer(first_name="Tom", family_name="Ford", age=57)
+# tom.age  # 57 という値を返す
 
 # ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
-# ieyasu.age # 73 という値を返す
+# ieyasu.age  # 73 という値を返す
 
 # print(ken.age)
 # print(tom.age)
@@ -95,12 +96,13 @@ print(ieyasu.info_csv())
 
 # -------------------c-1 start-----------------------------------
 # class Customer:
-#     def __init__(self, first_name,family_name):
+#     def __init__(self, first_name, family_name):
 #         self.first_name = first_name
 #         self.family_name = family_name
 
 #     def full_name(self):
 #         return f"{self.first_name} {self.family_name}"
+
 
 # ken = Customer(first_name="Ken", family_name="Tanaka")
 # ken.full_name()  # "Ken Tanaka" という値を返す
